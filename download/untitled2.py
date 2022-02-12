9cb06b253cb6428a85713cab3854078b71512840
[Github <[Github>
2022-02-12 16:49:09 +0800

    a

---
+++ download/untitled2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  7 06:29:19 2022
+
+@author: k
+"""
+import websocket
+import json
+import time
+
+ws = websocket.WebSocket()
+ws.connect('ws://localhost:8000/ws/test1')
+
+
+while(True):
+    time.sleep(2)
+    exec(open("complete 2.py").read())
+    
+     
